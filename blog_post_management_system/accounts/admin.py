09b85a9f216c0d3606b8b5b8cfa5a9df6d7e2698
Vivek Soni <vivek1.citrusbug@gmail.com>
2024-11-27 18:02:32 +0530

    Day end commit

--- blog_post_management_system/accounts/admin.py
+++ blog_post_management_system/accounts/admin.py
@@ -1,4 +1,8 @@
 from django.contrib import admin
 from .domain.models import UserProfile
 
-admin.site.register(UserProfile)
+class AdminProfileClass(admin.ModelAdmin):
+    list_display = ('user','bio','profile_picture')
+    
+
+admin.site.register(UserProfile,AdminProfileClass)
--- blog_post_management_system/blogs/admin.py
+++ blog_post_management_system/blogs/admin.py
@@ -4,20 +4,29 @@ from blogs.domain.models import BlogPost
 from comments.domain.models import UserComments
 from likes.domain.models import Like
 
+
 class CommentInline(admin.TabularInline):
+    """Admin comment class for inline functionality"""
+
     model = UserComments
     extra = 0
-    
+
+
 class LikesInline(admin.TabularInline):
+    """Admin Like class for inline functionality"""
+
     model = Like
-    extra= 0
+    extra = 0
+
 
 # Register your models here.
 class BlogAdmin(admin.ModelAdmin):
-    inlines = [CommentInline,LikesInline]
-    list_display = ['title','content','author', 'date_published']
-    list_filter = ['author', 'date_published']
-    search_fields = ['title','content','author', 'date_published']
-    
+    """Class handles view of BlogPost model in Admin site"""
 
-admin.site.register(BlogPost,BlogAdmin)
+    inlines = [CommentInline, LikesInline]
+    list_display = ["title", "content", "author", "date_published"]
+    list_filter = ["author", "date_published"]
+    search_fields = ["title", "content", "author", "date_published"]
+
+
+admin.site.register(BlogPost, BlogAdmin)
